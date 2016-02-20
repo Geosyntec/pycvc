ca1dbe2ce4746dddba57a2d03cf5f6197231d097
Paul Hobson <pmhobson@gmail.com>
2016-02-19 16:21:26 -0800

    rest of the hydrooviz tests

--- pycvc/tests/viz_tests.py
+++ pycvc/tests/viz_tests.py
@@ -33,3 +33,45 @@ def test_hydro_pairplot():
     fig4 = viz.hydro_pairplot(hydro, 'ED-1', by='grouped_season', save=False).fig
 
 
+@image_comparison(
+    baseline_images=[
+        'test_hydro_histogram_site_hasoutflow',
+        'test_hydro_histogram_site_year',
+        'test_hydro_histogram_single_site',
+    ],
+    extensions=['png']
+)
+def test_hydro_histogram():
+    hydro = load_test_data('test_hydro.csv', parse_dates=['start_date', 'end_date'])
+    viz.hydro_histogram(hydro, hue='site', row='has_outflow', save=False).fig
+    viz.hydro_histogram(hydro, col='site', hue='year', col_wrap=2, save=False).fig
+    viz.hydro_histogram(hydro.query("site == 'ED-1'"), palette='Blues', save=False).fig
+
+
+@image_comparison(
+    baseline_images=[
+        'test_hydro_jointplot_depth_outflow',
+        'test_hydro_jointplot_antecdays_outflow',
+    ],
+    extensions=['png']
+)
+def test_hydro_jointplot():
+    hydro = load_test_data('test_hydro.csv', parse_dates=['start_date', 'end_date'])
+    viz.hydro_jointplot(
+        hydro=hydro, site='ED-1',
+        xcol='total_precip_depth',
+        ycol='outflow_mm',
+        one2one=True,
+        color='b',
+        save=False
+    )
+
+    viz.hydro_jointplot(
+        hydro=hydro, site='LV-4',
+        xcol='antecedent_days',
+        ycol='outflow_mm',
+        conditions="outflow_mm > 0",
+        one2one=False,
+        color='g',
+        save=False
+    )

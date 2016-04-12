ea4837e2a5bcd431b6ac831f721a5bc12a93851b
Paul Hobson <PHobson@GeoSyntec.net>
2016-04-12 16:49:58 -0700

    update version numbers and notebooks

--- setup.py
+++ setup.py
@@ -1,30 +1,21 @@
-# Setup script for the pybmp package
+# Setup script for the pycvc package
 #
 # Usage: python setup.py install
 #
 import os
 from setuptools import setup, find_packages
 
-
-def getDataFiles(datadir):
-    #datadir = os.path.join(folder, subfolder)
-    files = [d for d in map(
-        lambda x: os.path.join(datadir, x),
-        os.listdir(datadir)
-    )]
-    return files
-
 DESCRIPTION = "pycvc: Analyze CVC stormwater data"
 LONG_DESCRIPTION = DESCRIPTION
 NAME = "pycvc"
-VERSION = "0.1.1"
+VERSION = "0.3.0"
 AUTHOR = "Paul Hobson (Geosyntec Consultants)"
 AUTHOR_EMAIL = "phobson@geosyntec.com"
 URL = ""
 DOWNLOAD_URL = ""
 LICENSE = "BSD 3-clause"
 PACKAGES = find_packages(exclude=[])
-PLATFORMS = "Python 2.7, 3.3 and later."
+PLATFORMS = "Python 3.4 and later."
 CLASSIFIERS = [
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
@@ -33,7 +24,6 @@ CLASSIFIERS = [
     "Topic :: Formats and Protocols :: Data Formats",
     "Topic :: Scientific/Engineering :: Earth Sciences",
     "Topic :: Software Development :: Libraries :: Python Modules",
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
 ]
